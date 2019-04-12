b33bff439c23d1d95834f97f67e0d10552d27f06
danielefongo <danielefongo@gmail.com>
2019-04-12 14:10:29 +0200

    first commit

---
+++ src/main/kotlin/com/bodfod/issuesdashboard/IssuesDashboardApplication.kt
@@ -0,0 +1,11 @@
+package com.bodfod.issuesdashboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IssuesDashboardApplication
+
+fun main(args: Array<String>) {
+	runApplication<IssuesDashboardApplication>(*args)
+}
